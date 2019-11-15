845abc1c431a3df86dea67b999007d3dd64715ee
github username <github email address>
2019-11-15 13:47:46 +0000

    Done.

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,22 @@
 // your ImageSlider code here!
+import React, {Component} from 'react';
+
+class ImageSlider extends React.Component {
+
+    state = {
+        currentSlideIndex: 0
+
+    }
+
+
+
+    render() {
+        return ( 
+            <div>
+            I am on slide {this.state.currentSlideIndex}
+            </div>
+        )
+    }
+}
+
+export default ImageSlider
